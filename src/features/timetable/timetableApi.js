21c51e94b830c0f4bd63392cb38cf4c87035aa18
stK04 <357kostya3572004@gmail.com>
2024-05-14 21:56:31 +0300

    Added Timetable

---
+++ src/features/timetable/timetableApi.js
@@ -0,0 +1,25 @@
+import {createApi} from "@reduxjs/toolkit/query/react";
+import {getBaseQuery} from "../config.js";
+
+export const timetableApi = createApi({
+    reducerPath: 'timetableApi',
+    baseQuery: getBaseQuery('timetable'),
+    endpoints: (builder) => ({
+        getTimeTable: builder.query({
+            query: (groupId) => `/get_by_group/${groupId}/current`,
+            // onQueryStarted: async (_, {dispatch, queryFulfilled}) => {
+            //     try {
+            //         const {data} = await queryFulfilled;
+            //         console.log(data)
+            //         dispatch(setUser({user: data}))
+            //     } catch(e) {
+            //         dispatch(logoutUser())
+            //     }
+            // }
+        }),
+
+
+    }),
+});
+
+export const {useGetTimeTableQuery} = timetableApi;
--- src/pages/Dashboard/MainPage/MainPage.jsx
+++ src/pages/Dashboard/MainPage/MainPage.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+    import { useSelector } from 'react-redux';
+    import { useGetTimeTableQuery } from '../../../features/timetable/timetableApi';
+    import {
+        Table,
+        TableBody,
+        TableCell,
+        TableContainer,
+        TableHead,
+        TableRow,
+        Paper,
+        Typography,
+        CircularProgress,
+        Container
+    } from '@mui/material';
 
-function MainPage() {
-    return (
-        <div>
-            MainPage
-        </div>
-    );
-}
+    function MainPage() {
+        const user = useSelector(store => store.user.user);
+        const { data = {}, isLoading } = useGetTimeTableQuery(user.usergroup.id);
+        console.log(data);
 
-export default MainPage;
+        if (isLoading) {
+            return <div><CircularProgress /></div>;
+        }
+
+        const {upper_lower_week, ...days} = data
+
+        return (
+            <Container>
+                Тиждень: <b>{upper_lower_week.toLowerCase() === 'верхня' ? 'верхній' : 'нижній'}</b>
+                {Object.keys(days).map((day, key) => (
+                    <div key={key}>
+                        <Typography variant="h6" gutterBottom>{day}</Typography>
+                        <TableContainer component={Paper} style={{ marginBottom: '20px' }}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Номер заняття</TableCell>
+                                        <TableCell>Назва заняття</TableCell>
+                                        <TableCell>Тип заняття</TableCell>
+                                        <TableCell>Викладач</TableCell>
+                                        <TableCell>Посилання</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {Object.keys(data[day]).map((lessonNumber, lessonKey) => (
+                                        <TableRow key={lessonKey}>
+                                            <TableCell>{lessonKey + 1}</TableCell>
+                                            <TableCell>{data[day][lessonNumber]?.lesson_name || '-'}</TableCell>
+                                            <TableCell>{data[day][lessonNumber]?.lesson_type || '-'}</TableCell>
+                                            <TableCell>{data[day][lessonNumber]?.teacher || '-'}</TableCell>
+                                            <TableCell>{data[day][lessonNumber]?.url ? <a href={data[day][lessonNumber]?.url}>Посилання</a> : '-'}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+                ))}
+            </Container>
+        );
+    }
+
+    export default MainPage;

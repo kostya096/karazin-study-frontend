feb88152a9e32018f71a1e27fdb1a60bcb5d4082
stK04 <357kostya3572004@gmail.com>
2024-05-14 21:57:32 +0300

    Added new apis

--- src/features/store.js
+++ src/features/store.js
@@ -2,15 +2,22 @@ import {configureStore} from '@reduxjs/toolkit';
 import {userSlice} from "./user/userSlice.js";
 import {userApi} from "./user/userApi.js";
 import {adminSlice} from "./admin/slices.js";
+import {coursesApi} from "./courses/coursesApi.js";
+import {timetableApi} from "./timetable/timetableApi.js";
 
 export const store = configureStore({
     reducer: {
         [userSlice.name]: userSlice.reducer,
         [userApi.reducerPath]: userApi.reducer,
+        [coursesApi.reducerPath]: coursesApi.reducer,
+        [timetableApi.reducerPath]: timetableApi.reducer,
         admin: adminSlice.reducer
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(userApi.middleware),
+        getDefaultMiddleware()
+            .concat(userApi.middleware)
+            .concat(coursesApi.middleware)
+            .concat(timetableApi.middleware),
 
 });
 
